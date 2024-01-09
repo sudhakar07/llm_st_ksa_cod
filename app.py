692821a6977e42f445bad7aaafdf4d5aa62c6e19
sudhakar07 <sudhakargk74@gmail.com>
2024-01-09 16:14:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
       if user_question:
         docs = knowledge_base.similarity_search(user_question)
         
-        llm = OpenAI()
+        llm = OpenAI({modelName: "gpt-3.5-turbo-instruct"})
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
